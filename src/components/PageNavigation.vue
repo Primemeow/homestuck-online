e4843f724e2e25577a588a665f24d2fd4f536cb9
Makin <makin@protonmail.com>
2023-07-16 19:46:20 +0200

    first big CSS fix, it looks like real home stuck now!!!!!

--- src/components/PageNavigation.vue
+++ src/components/PageNavigation.vue
@@ -3,7 +3,7 @@
         <div class="nextArrow" v-if="nextPages.length > 0">
             <div v-for="(page, index) in nextPages" :key="index">
                 <p v-if="('pageId' in page && 'title' in page)">
-                &gt; <a :href="commandLink(page)" class="nextArrowLink" v-html="commandText(page)" @click="$updateNewReader(page.pageId)" />
+                &gt; <a :href="commandLink(page)" class="nextArrowLink" v-html="commandText(page)"/>
                 </p>
             </div>
         </div>
@@ -17,8 +17,8 @@
                 </li>
             </ul>
             <ul class="meta">
-                <li v-if="'timestamp' in thisPage && !!getTimestamp()">
-                    <div class="timestamp" :data-timestamp="thisPage.timestamp" v-html="getTimestamp()" />
+                <li v-if="'timestamp' in thisPage && pageTimestamp">
+                    <div class="timestamp" :data-timestamp="thisPage.timestamp" v-html="pageTimestamp" />
                 </li>
             </ul>
         </div>
@@ -26,7 +26,7 @@
 </template>
   
 <script setup>
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
 
 const props = defineProps(["thisPage", "nextPages", "startOverLink", "previousPageLink", "story"]);
 
@@ -38,11 +38,9 @@ function commandLink(page) {
     return `/${props.story}/${page.next[0]}`;
 }
 
-function getTimestamp() {
+const pageTimestamp = computed(() => {
     // Returns a formatted timestamp OR undefined
-    if (!props.thisPage.timestamp) {
-        return undefined;
-    } else {
+    if (props.thisPage.timestamp) {
         try {
             const seconds = Number(props.thisPage.timestamp);
             const date = new Date(seconds * 1000);
@@ -53,142 +51,47 @@ function getTimestamp() {
             this.$logger.error(e);
             return "Invalid Date";
         }
+    } else {
+        return undefined;
     }
-};
+});
 </script>
 
 
 <style scoped lang="scss">
-  .pageBody {
-    color: var(--font-default);
-    background: var(--page-pageBody);
-
-    margin: 0;
-    padding: 0;
+  .nextArrow{
+    margin: 0 0 30px 0;
+    font-family: Verdana, Arial, Helvetica, sans-serif;
+    font-size: x-large;
+    font-weight: normal;
+  }
+  .footerNav{
+    overflow: hidden;
     display: flex;
-    flex: 1 0 auto;
-    flex-flow: column;
-    align-items: center;
-
-    > img {
-      align-self: center;
-    }
-
-    &.supercartridge {
-      .pageFrame {
-        padding-top: 0;
-        .pageContent {
-          max-width: 650px;
+    justify-content: space-between;
+    line-height: 10px;
+    ul {
+      list-style: none;
+      font-size: 10px;
+      font-family: Verdana, Arial, Helvetica, sans-serif;
+      li {
+        float: left;
+        padding-bottom: 15px;
+        * {display: inline-block;}
+        &:not(:last-child):after {
+          content: "|";
+          margin: 0 0.3em
         }
       }
     }
-
-    &.hscroll {
-      .pageFrame, .navBanner {
-        width: 1200px !important;
-
-        .pageContent {
-          max-width: 1100px;
-
-          .media {
-            align-items: flex-start !important;
-            overflow-x: scroll;
-            width: 1100px;  
-
-            &:after{
-              content: url(assets://scraps2/hscrollarrow.gif);
-            }
-          }   
-        }
+    .meta {
+      font-weight: 300;
+      color: var(--page-nav-meta);
+      a {
+        text-decoration: none;
       }
-    }
-
-    &.scratchIntermission {
-      .navBanner {
-        width: 940px;
-      }
-      .pageFrame {
-        width: 940px;
-      }
-    }
-
-    //Small screen check
-    @media only screen and (max-width: 850px) {
-      &{
-        overflow-x: hidden;
-        height: max-content;
-        .navBanner {
-          max-width: 100%;
-        }
-        .pageFrame {
-          max-width: 100%;
-        }
-        ::v-deep div.footer {
-          max-width: 100%;
-        }
-      }
-    }
-
-    .pageFrame {
-      background: var(--page-pageFrame);
-
-      width: 950px;
-      padding-top: 7px;
-      padding-bottom: 23px;
-      margin: 0 auto;
-      position: relative; // Allow things to align to the page
-
-      flex: 0 1 auto;
-      display: flex;
-      justify-content: center;
-
-      .pageContent {
-        background: var(--page-pageContent);
-
-        max-width: 950px;
-        min-width: 650px;
-        display: flex;
-        flex: 0 1 auto;
-        align-items: center;
-        flex-flow: column;
-
-        .footnotesContainer {
-          width: 100%;
-        }
-
-        .mediaContent {
-          display: flex;
-          align-items: center;
-          flex-flow: column;
-
-          h2.pageTitle {
-            max-width: 590px;
-            text-align: center;
-            line-height: 1.1;
-            font-size: 32px;
-            padding: 15px 0;
-          }
-
-          .media {
-            display: flex;
-            align-items: center;
-            flex-flow: column;
-
-            .panel {
-              &:not(:last-child) {
-                margin-bottom: 20px;
-              }
-            }            
-          }
-        }
-
-        .textContent{
-          margin-top: 30px;
-          width: 600px;
-          display: flex;
-          flex-direction: column;
-          
-        }
+      a:hover {
+        text-decoration: underline;
       }
     }
   }
--- src/views/MspaPage.vue
+++ src/views/MspaPage.vue
@@ -1,22 +1,21 @@
 <template>
   <div v-if="!isLoading" class="pageBody" :class="pageBodyClasses">
-    <div class="mediaContent">
-      <h2 class="pageTitle" v-text="pageData.title" v-if="!isSupercartridge" ref="pageTitle"/>
-      <div class="media" ref="media">
-        <media-embed v-for="url in pageMedia" :key="url" :url="url" class="mediaElement"/>
+    <div class="pageFrame">
+      <div class="pageContent">
+        <div class="mediaContent">
+          <h2 class="pageTitle" v-text="pageData.title" v-if="!isSupercartridge" ref="pageTitle"/>
+          <div class="media" ref="media">
+            <media-embed v-for="url in pageMedia" :key="url" :url="url" class="mediaElement"/>
+          </div>
+        </div>
+        <div class="textContent">
+          <page-text v-if="pageData.content" :content="pageData.content" :page-id="pageData.pageId"/>
+          <page-navigation :this-page="store.currentPage" :next-pages="nextPagesArray" 
+          :previous-page-link="previousPageLink" :start-over-link="startOverLink" :story="story"
+          ref="pageNav" :class="{'hidden': hidePageNav}" />
+        </div>
       </div>
     </div>
-    <div class="textContent">
-      <page-text v-if="pageData.content" :content="pageData.content" :page-id="pageData.pageId"/>
-      <page-navigation :this-page="store.currentPage" :next-pages="nextPagesArray" 
-      :previous-page-link="previousPageLink" :start-over-link="startOverLink" :story="story"
-      ref="pageNav" :class="{'hidden': hidePageNav}" />
-    </div>
-    <div class="command">
-      <router-link :to="nextPageLink">{{ nextPageTitle }}</router-link>
-    </div>
-    <router-link :to="startOverLink">Start Over</router-link>
-    <router-link :to="previousPageLink">Go Back</router-link>
   </div>
   <div v-else>Loading...</div>
 </template>
@@ -180,4 +179,137 @@ onMounted(() => {
 
 <style lang="scss" scoped>
 
+.pageBody {
+  color: var(--font-default);
+  background: var(--page-pageBody);
+
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex: 1 0 auto;
+  flex-flow: column;
+  align-items: center;
+
+  > img {
+    align-self: center;
+  }
+
+  &.supercartridge {
+    .pageFrame {
+      padding-top: 0;
+      .pageContent {
+        max-width: 650px;
+      }
+    }
+  }
+
+  &.hscroll {
+    .pageFrame, .navBanner {
+      width: 1200px !important;
+
+      .pageContent {
+        max-width: 1100px;
+
+        .media {
+          align-items: flex-start !important;
+          overflow-x: scroll;
+          width: 1100px;  
+
+          &:after{
+            content: url(assets://scraps2/hscrollarrow.gif);
+          }
+        }   
+      }
+    }
+  }
+
+  &.scratchIntermission {
+    .navBanner {
+      width: 940px;
+    }
+    .pageFrame {
+      width: 940px;
+    }
+  }
+
+  //Small screen check
+  @media only screen and (max-width: 850px) {
+    &{
+      overflow-x: hidden;
+      height: max-content;
+      .navBanner {
+        max-width: 100%;
+      }
+      .pageFrame {
+        max-width: 100%;
+      }
+      :deep(div.footer) {
+        max-width: 100%;
+      }
+    }
+  }
+
+  .pageFrame {
+    background: var(--page-pageFrame);
+
+    width: 950px;
+    padding-top: 7px;
+    padding-bottom: 23px;
+    margin: 0 auto;
+    position: relative; // Allow things to align to the page
+
+    flex: 0 1 auto;
+    display: flex;
+    justify-content: center;
+
+    .pageContent {
+      background: var(--page-pageContent);
+
+      max-width: 950px;
+      min-width: 650px;
+      display: flex;
+      flex: 0 1 auto;
+      align-items: center;
+      flex-flow: column;
+
+      .footnotesContainer {
+        width: 100%;
+      }
+
+      .mediaContent {
+        display: flex;
+        align-items: center;
+        flex-flow: column;
+
+        h2.pageTitle {
+          max-width: 590px;
+          text-align: center;
+          line-height: 1.1;
+          font-size: 32px;
+          padding: 15px 0;
+        }
+
+        .media {
+          display: flex;
+          align-items: center;
+          flex-flow: column;
+
+          .panel {
+            &:not(:last-child) {
+              margin-bottom: 20px;
+            }
+          }            
+        }
+      }
+
+      .textContent{
+        margin-top: 30px;
+        width: 600px;
+        display: flex;
+        flex-direction: column;
+        
+      }
+    }
+  }
+}
 </style>
